4f0724249778a76c5b598a33fe3e074ad6f03ad5
Eslam-Nazer <eslam.nazer43@gmail.com>
2026-02-12 01:22:20 +0200

    Setup jpa and remove jdbc with implementions

---
+++ src/main/java/com/devtrio/database/repositories/AuthorRepository.java
@@ -0,0 +1,4 @@
+package com.devtrio.database.repositories;
+
+public interface AuthorRepository {
+}
---
+++ src/main/java/com/devtrio/database/repositories/BookRepository.java
@@ -0,0 +1,4 @@
+package com.devtrio.database.repositories;
+
+public interface BookRepository {
+}

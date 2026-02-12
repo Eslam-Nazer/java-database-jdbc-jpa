43d9bb5c2b3aef1bf00e065bdc49065fbba7e65f
Eslam-Nazer <eslam.nazer43@gmail.com>
2026-02-12 17:00:15 +0200

    Refactor test cases to jpa and repositories function

--- src/test/java/com/devtrio/database/TestDataUtil.java
+++ src/test/java/com/devtrio/database/TestDataUtil.java
@@ -1,10 +1,8 @@
 package com.devtrio.database;
 
-import com.devtrio.database.dao.AuthorDao;
 import com.devtrio.database.domain.Author;
 import com.devtrio.database.domain.Book;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import com.devtrio.database.repositories.AuthorRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +11,6 @@ public class TestDataUtil {
 
     public static Author createTestAuthor() {
         return Author.builder()
-                .id(1L)
                 .name("Abigail Rose")
                 .age(25)
                 .build();
@@ -22,45 +19,45 @@ public class TestDataUtil {
     public static List<Author> createTestAuthors() {
         List<Author> authors = new ArrayList<Author>();
 
-        authors.add(Author.builder().id(1L).name("John Doe").age(38).build());
-        authors.add(Author.builder().id(2L).name("Dara Tom").age(28).build());
-        authors.add(Author.builder().id(3L).name("Sara Don").age(24).build());
+        authors.add(Author.builder().name("John Doe").age(38).build());
+        authors.add(Author.builder().name("Dara Tom").age(28).build());
+        authors.add(Author.builder().name("Sara Don").age(24).build());
 
         return authors;
     }
 
-    public static Book createTestBook() {
+    public static Book createTestBook(Author author) {
+
         return Book.builder()
                 .isbn("978-1-2345-6789-0")
                 .title("The Shadow in the Attic")
-                .authorId(1L)
+                .author(author)
                 .build();
     }
 
-    public static List<Book> createTestBooks(AuthorDao authorDao) {
+    public static List<Book> createTestBooks() {
         List<Book> books = new ArrayList<Book>();
 
         Author author = createTestAuthor();
-        authorDao.create(author);
 
         books.add(Book.builder()
                 .isbn("978-1-2345-6789-1")
                 .title("The Shadow in the Attic")
-                .authorId(author.getId())
+                .author(author)
                 .build()
         );
 
         books.add(Book.builder()
                 .isbn("978-1-2345-6789-2")
                 .title("Beyond the Horizon")
-                .authorId(author.getId())
+                .author(author)
                 .build()
         );
 
         books.add(Book.builder()
                 .isbn("978-1-2345-6789-3")
                 .title("The Last Ember")
-                .authorId(author.getId())
+                .author(author)
                 .build()
         );
 

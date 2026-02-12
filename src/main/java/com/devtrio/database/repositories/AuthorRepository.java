4aaea74b565d7fd2549a26a7fac0ab3266a0cbb9
Eslam-Nazer <eslam.nazer43@gmail.com>
2026-02-12 18:48:23 +0200

    Custom query and function using @Qeury() annotition with query

--- src/main/java/com/devtrio/database/repositories/AuthorRepository.java
+++ src/main/java/com/devtrio/database/repositories/AuthorRepository.java
@@ -1,10 +1,14 @@
 package com.devtrio.database.repositories;
 
 import com.devtrio.database.domain.Author;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorRepository extends CrudRepository<Author, Long> {
     Iterable<Author> ageLessThan(int age);
+
+    @Query("SELECT a from Author a WHERE a.age > ?1")
+    Iterable<Author> findAuthorsWithGreaterThan(int age);
 }
